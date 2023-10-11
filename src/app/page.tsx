9b5cabe9ce605bbc983d18ab41b4b689528f047f
swarnadeep <aritrasarkar2002@gmail.com>
2023-10-11 19:06:36 +0530

    test-1

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import GenerateSoundView from "./views/GenerateSoundView";
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      
+      {/* Render the GenerateSoundView component */}
+      <GenerateSoundView />
+    </main>
+  );
+}
